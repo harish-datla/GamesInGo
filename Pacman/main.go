2b31b950d64ff09316b92c78a4f393253b4f7bf5
harish-datla <36021808+harish-datla@users.noreply.github.com>
2023-03-28 21:08:35 +0530

    feat: adding mazetxt file and basic outline of the game

---
+++ Pacman/main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main(){
+	// Standard steps in game, more or less
+
+	// 1 - Initialize game
+ 
+	// 2 - Load and render graphics
+
+	// 3 - Game loop
+
+}
