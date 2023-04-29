f630646e427dfdb86fcc7473235b70447f72a5ad
harish-datla <36021808+harish-datla@users.noreply.github.com>
2023-04-29 09:26:07 +0530

    feat: basic functionality to render graphics

--- Pacman/main.go
+++ Pacman/main.go
@@ -7,13 +7,56 @@ import(
 	"os"
 )
 
+
+func loadMaze(fp *os.File) []string{
+	scanner := bufio.NewScanner(fp)
+	var maze []string
+	for scanner.Scan(){
+		newLine := scanner.Text()
+		maze = append(maze,newLine)
+	}
+	return maze
+}
+
+func loadMazeFromTxtFile() ([]string,error) {
+	mazeTxtFilePath := "maze_ascii.txt"
+
+	//openFile
+	fp,err := os.Open(mazeTxtFilePath)
+	if err != nil {
+		return nil,err
+	}
+	defer fp.Close()
+
+	//createMaze and return it
+	return loadMaze(fp)
+}
+
+
+func printScreen(maze){
+	for _,line := range maze{
+		fmt.Println(line)
+	}
+}
+
 func main(){
 	// Standard steps in game, more or less
 
 	// 1 - Initialize game
  
 	// 2 - Load and render graphics
+	maze,err := loadMaze()
+	if err != nil{
+		log.Printf("Error rendering graphics: %v", err)
+	}
 
 	// 3 - Game loop
+	for{
+		
+		printScreen(maze)
+
+
+		break
+	}
 
 }
